3bfd685bb03f386adb3a7b1f16432b784f4766b7
Fernilicious <reyesfernando151@gmail.com>
2022-10-18 10:34:46 -0700

    working on login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from "./components/Home";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Posts from "./components/Post"
+import Login from "./components/Login"
 
 // import { useState, useEffect} from "react"
 
@@ -13,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/post" element={<Posts/>} />
-        {/* <Route path="/post/postId" element={<SinglePost/>} */}
+        <Route path="/Login" element={<Login/>}/>
       </Routes>
      
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,10 @@ export default function Navbar() {
     return (
         <div>
             <Link to="/">Homepage</Link>
+
             <Link to="/post">POSTS</Link>
+            
+            <Link to ="/Login">Login</Link>
             
 
         </div>
