d8f0aa7d3f8830eb65ea3c8009f9f25431e62c15
Fernilicious <reyesfernando151@gmail.com>
2022-10-20 13:16:47 -0700

    Start of serach bar

--- src/components/Postdetails.jsx
+++ src/components/Postdetails.jsx
@@ -1,33 +1,30 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { fetchPosts } from "../api/auth";
 
-
 const SpecificPost = () => {
-
-
   const { id } = useParams();
 
   const [singlePost, setSinglePost] = useState({});
 
-  // get fetchposts 
+  // get fetchposts
 
   useEffect(() => {
     async function loadPosts() {
       const result = await fetchPosts();
-      console.log("posts", result.data.posts)
+      console.log("posts", result.data.posts);
       const posts = result.data.posts;
       const [post] = posts.filter((specPost) => {
         return specPost._id === id;
-      })
+      });
       setSinglePost(post);
     }
 
     loadPosts();
   }, []);
 
-  //filter through posts to get param 
+  //filter through posts to get param
   return (
     <div>
       <h2>{singlePost.title}</h2>
@@ -35,14 +32,11 @@ const SpecificPost = () => {
       <h2>{singlePost.price}</h2>
       <h3>{singlePost.description}</h3>
       <h3>{singlePost.author.username}</h3>
-
     </div>
-  )
-
-}
+  );
+};
 //set the state pf singlePost to those params
-
 
 //return display of more details
 
-export default SpecificPost; 
+export default SpecificPost;
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,34 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { fetchPosts } from "../api/auth";
 
+function Searchbar() {
+  const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-function Searchbar(post, text) {
-    const [posts, setPosts] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    useEffect(() => {
-        async function loadPosts() {
-            const result = await fetchPosts();
-            setPosts(result.data.posts);
-        }
+  useEffect(() => {
+    async function loadPosts() {
+      const result = await fetchPosts();
+      setPosts(result.data.posts);
+      console.log(result.data.posts);
+    }
 
-        loadPosts();
-    }, []);
+    loadPosts();
+  }, []);
 
-    return (
-        <div>
-            <form>
-                <input type="text" placeholder="Search for Posts" />
-                <button type="submit">Search</button>
-            </form>
-        </div>
-    )
-
+  function postMatches(post, text) {
     // return true if any of the fields you want to check against include the text
-    // strings have an .includes() method 
+    // strings have an .includes() method
+  }
+
+  const filteredPosts = posts.filter((post) => postMatches(post, searchTerm));
+  const postsToDisplay = searchTerm.length ? filteredPosts : posts;
+
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Search for Posts" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
 
-const filteredPosts = posts.filter(post => postMatches(post, searchTerm));
-const postsToDisplay = searchTerm.length ? filteredPosts : posts;
 // then, in our jsx below... map over postsToDisplay instead of posts
 
-export default Searchbar; 
+export default Searchbar;
