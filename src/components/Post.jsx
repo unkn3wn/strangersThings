f0737a674816839a01a3f19ed601aee68e25b2fd
unkn3wn <reyesfernando151@gmail.com>
2023-01-23 10:27:14 -0800

    fixed way post looked

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import { fetchPosts } from "../api/auth";
 import CreateNewPost from "../components/Createpost";
 import { useNavigate } from "react-router-dom";
-import styles from "../ComponentCss/Post.module.css"
+import styles from "../ComponentCss/Post.module.css";
 
 export default function Posts() {
   const [posts, setPosts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   const navigate = useNavigate();
 
@@ -21,41 +21,40 @@ export default function Posts() {
   function postMatches(post, text) {
     return post.title.toLowerCase().includes(text);
   }
-  const filteredPosts = posts.filter(post => postMatches(post, searchTerm));
+
+  const filteredPosts = posts.filter((post) => postMatches(post, searchTerm));
   const postsToDisplay = searchTerm.length ? filteredPosts : posts;
   return (
-    <div key={posts._id}>
-      <h5 className={styles.searchbar}>Please search in lowercase!</h5>
-      <input className={styles.searchbarr}
-        type="text"
-        value={searchTerm}
-        placeHolder="Search"
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <CreateNewPost />
-      {postsToDisplay.map((post) => {
-        return (
-          <div className={styles.postlist} key={post.title}>
-
-
-            <div />
-            <h3>{post.title}</h3>
-            <h4>{post.price}</h4>
-
-            <button
-              onClick={() => {
-                navigate(`/posts/${post._id}`);
-              }}
-            >
-              See Details
-            </button>
-
-
-          </div>
-        );
-
-      })}
-    </div >
+    <div>
+      <div className={styles.searchslangInput}>
+        <h5 className={styles.searchSlang}>search for a post in lowercase</h5>
+        <input
+          className={styles.searchbarr}
+          type="text"
+          value={searchTerm}
+          placeHolder="Search"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      {/* 
+      <CreateNewPost /> */}
+      <div className={styles.postlist}>
+        {postsToDisplay.map((post) => {
+          return (
+            <div className={styles.cards} key={post.title}>
+              <h3>{post.title}</h3>
+              <h4>{post.price}</h4>
+              <button
+                onClick={() => {
+                  navigate(`/posts/${post._id}`);
+                }}
+              >
+                See Details
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
-
